506fb8e998bbd442af1ae25c9b10d35c891ad4c7
Aditi Vishwas Kamble <aditi.kamble.che22@itbhu.ac.in>
2024-07-05 14:29:46 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -11,4 +11,5 @@ logging.basicConfig(
   format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
   level=logging.INFO,
 )
-
+if __name__=="__main__":
+    logging.info("Logging has started.....")
