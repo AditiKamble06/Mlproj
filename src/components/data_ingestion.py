ad366cd017d375ecd5bb66a1b917192cbf7b3f80
Aditi Vishwas Kamble <aditi.kamble.che22@itbhu.ac.in>
2024-07-07 14:13:59 +0530

    new commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,11 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_training import ModelTrainingConfig
+from src.components.model_training import ModelTraining
 
 @dataclass
 class DataIngestionConfig:
@@ -18,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion....")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info('Read the data as dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -35,4 +40,8 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()              
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)  
+    modeltrainer=ModelTraining()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
